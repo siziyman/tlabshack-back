21f07b9ea90a6dd25adb83cbd2c99de2d29d2080
Aleksei Belezeko <aleksei.belezeko@t-systems.com>
2019-01-20 03:51:42 +0300

    Implement request ride method

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"firebase.google.com/go"
-	"firebase.google.com/go/messaging"
-	"github.com/julienschmidt/httprouter"
-	"google.golang.org/api/option"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/siziyman/tlabshack-back/rides"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
+	"github.com/julienschmidt/httprouter"
+	"google.golang.org/api/option"
 )
 
 type State struct {
@@ -98,7 +101,7 @@ func main() {
 	router := httprouter.New()
 	router.POST("/registerDevice", registerDevice)
 	router.POST("/registerDriver", drive)
-	router.POST("/availableRides", seekRide)
+	router.GET("/availableRides", seekRide)
 	log.Fatal(http.ListenAndServe("10.177.1.130:8080", router))
 }
 
@@ -126,6 +129,22 @@ func seekRide(writer http.ResponseWriter, request *http.Request, params httprout
 		return
 	}
 
+	for _, driver := range State.DriverMetadata {
+		first := rides.Distance(driver.RideData.StartPoint.Latitude, driver.RideData.StartPoint.Longitude, rideRequest.RideData.StartPoint.Latitude, rideRequest.RideData.StartPoint.Longitude)
+		second := rides.Distance(driver.RideData.EndPoint.Latitude, driver.RideData.EndPoint.Longitude, rideRequest.RideData.EndPoint.Latitude, rideRequest.EndPoint.Longitude)
+		if first+second < 1000 {
+			for _, device := range State.DeviceMetadata {
+				if device.Wallet == driver.Wallet {
+					sendPush(device.TokenId, rideRequest.Message)
+					writer.WriteHeader(200)
+					_, _ = writer.Write([]byte("Ride request was sent"))
+					return
+				}
+			}
+		}
+	}
+	writer.WriteHeader(404)
+	_, _ = writer.Write([]byte("Drivers not found"))
 }
 
 func registerDevice(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -146,4 +165,26 @@ func registerDevice(w http.ResponseWriter, r *http.Request, ps httprouter.Params
 	_, _ = w.Write([]byte("Successfully registered"))
 	//state.FirebaseApp.
 
+}
+
+func sendPush(registrationToken, message string) {
+	// Obtain a messaging.Client from the App.
+	if err != nil {
+		log.Fatalf("error getting Messaging client: %v\n", err)
+	}
+
+	// See documentation on defining a message payload.
+	message := &messaging.Message{
+		Data: map[string]string{
+			"message": message,
+		},
+		Token: registrationToken,
+	}
+
+	// Send a message to the device corresponding to the provided
+	// registration token.
+	response, err := State.MessagingClient.Send(ctx, message)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
